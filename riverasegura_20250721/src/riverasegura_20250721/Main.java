8466947da292d9d13f9f8cbed6a0cab4b0ab6474
AlejandroRivera-UFG <ia.aleriv@ufg.edu.sv>
2025-07-22 03:10:40 -0600

    Finalización de práctica

--- riverasegura_20250721/src/riverasegura_20250721/Main.java
+++ riverasegura_20250721/src/riverasegura_20250721/Main.java
@@ -15,14 +15,21 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("50 Grados Centigrados a Farenheit: " + celsius_a_farenheit() + " F");
+        System.out.println("50 Grados Centigrados a Fahrenheit: " + celsius_a_fahrenheit() + " F");
         System.out.println("50 Grados Centigrados a Kelvin: " + celsius_a_kelvin() + " K");
-        System.out.println("50 Grados Centigrados a Rankine: " + celsius_a_rankine() + " R");
-        System.out.println("30 Grados Farenheit a Celsius: " + farenheit_a_celsius() + " C");
-        System.out.println("30 Grados Farenheit a Kelvin: " + farenheit_a_kelvin() + " K");
+        System.out.println("50 Grados Centigrados a Rankine: " + celsius_a_rankine() + " Ra");
+        System.out.println("30 Grados Fahrenheit a Celsius: " + fahrenheit_a_celsius() + " C");
+        System.out.println("30 Grados Fahrenheit a Kelvin: " + fahrenheit_a_kelvin() + " K");
+        System.out.println("30 Grados Fahrenheit a Rankine: " + fahrenheit_a_rankine() + " Ra");
+        System.out.println("300 Grados Kelvin a Celsius: " + kelvin_a_celsius() + " C");
+        System.out.println("300 Grados Kelvin a Fahrenheit: " + kelvin_a_fahrenheit() + " F");
+        System.out.println("300 Grados Kelvin a Rankine: " + kelvin_a_rankine() + " Ra");
+        System.out.println("500 Grados Rankine a Celsius: " + rankine_a_celsius() + " C");
+        System.out.println("500 Grados Rankine a Fahrenheit: " + rankine_a_fahrenheit() + " F");
+        System.out.println("500 Grados Rankine a Kelvin: " + rankine_a_kelvin() + " K");
     }
     
-    public static double celsius_a_farenheit(){
+    public static double celsius_a_fahrenheit(){
         double grados_centigrados = 50.0;
         return ((grados_centigrados * 9) / 5) + 32;
     }
@@ -37,15 +44,50 @@ public class Main {
         return ((grados_centigrados * 9) / 5) + 491.67;
     }
     
-    public static double farenheit_a_celsius() {
+    public static double fahrenheit_a_celsius() {
         double grados_farenheit = 30.0;
         return ((grados_farenheit  - 32) * 5) / 9;
     }
     
-    public static double farenheit_a_kelvin() {
+    public static double fahrenheit_a_kelvin() {
         double grados_farenheit = 30.0;
-        return (((grados_farenheit  - 32) * 5) / 9) + 273.15;
+        return (((grados_farenheit  - 32) * 5) / 9) + 273.15;   
     }
+    
+    public static double fahrenheit_a_rankine(){
+        double grados_fahrenheit = 30.0 ;
+        return grados_fahrenheit + 459.67;
+    }
+         
+    public static double kelvin_a_celsius(){
+        double kelvin = 300.0;
+        return kelvin - 273.15;
+    }
+
+    public static double kelvin_a_fahrenheit() {
+        double kelvin = 300.0;
+        return ((kelvin * 9) / 5) - 459.67;
+    }
+
+    public static double kelvin_a_rankine() {
+        double kelvin = 300.0;
+        return (kelvin * 9) / 5;
+    }
+    public static double rankine_a_celsius() {
+        double rankine = 500.0;
+        return (rankine - 491.67) * 5 / 9;
+    }
+
+    public static double rankine_a_fahrenheit() {
+        double rankine = 500.0;
+        return rankine - 459.67;
+    }
+
+    public static double rankine_a_kelvin() {
+        double rankine = 500.0;
+        return rankine * 5 / 9;
+    }
+
 }
 
     
